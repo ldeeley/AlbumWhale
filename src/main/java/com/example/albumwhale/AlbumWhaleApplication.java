85c1de6957a017e7b43943d623e2052a0615978d
Lester Deeley <lester.deeley@yahoo.com>
2023-01-27 09:33:23 +0000

    Initial

---
+++ src/main/java/com/example/albumwhale/AlbumWhaleApplication.java
@@ -0,0 +1,13 @@
+package com.example.albumwhale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbumWhaleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlbumWhaleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/albumwhale/AlbumWhaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.albumwhale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbumWhaleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
